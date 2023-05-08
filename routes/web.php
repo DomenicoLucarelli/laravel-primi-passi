ea72d4c5a0509bf6a88dcc28087e1a94113c02a5
DomenicoLucarelli <domyluca@gmail.com>
2023-05-08 16:50:34 +0200

    feat : gestione dinamica

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    $user = 'Domenico Lucarelli';
+
+
+    return view('home', compact('user'));
 });
